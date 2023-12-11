0051c22bfe5791cac60cda63fdb4fd6f0c8efbf1
xiaoso <xiaoso456@qq.com>
2023-12-11 17:54:51 +0800

    draft

--- datastream-table-integration/src/main/java/io/github/xiaoso456/demo/flink/table/ChangeDemo.java
+++ datastream-table-integration/src/main/java/io/github/xiaoso456/demo/flink/table/ChangeDemo.java
@@ -2,13 +2,14 @@ package io.github.xiaoso456.demo.flink.table;
 
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.table.api.bridge.java.internal.StreamTableEnvironmentImpl;
+import org.apache.flink.types.Row;
 
-public class ElementsSourceDemo {
+public class ChangeDemo {
     public static void main(String[] args) throws Exception {
         // 基于本地模式，开启 web ui
         Configuration configuration = new Configuration();
@@ -22,10 +23,14 @@ public class ElementsSourceDemo {
         DataStreamSource<String> dataStreamSource = env.fromElements("spark", "flink", "flink", "hive");
         // dataStream转化为table
         Table inputTable = tableEnv.fromDataStream(dataStreamSource);
+        // 注册为临时table
+        tableEnv.createTemporaryView("InputTable", inputTable);
+        Table resultTable = tableEnv.sqlQuery("SELECT UPPER(f0) FROM InputTable");
 
+        // 把table转回dataStream
+        DataStream<Row> resultStream = tableEnv.toDataStream(resultTable);
+        resultStream.print();
 
-
-
-        env.execute("datagen print job");
+        env.execute("change job");
     }
 }
